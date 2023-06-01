431f0c056f51be0a0cda7d1e0821255ecf9c2618
Pop Maria <popmaria461@yahoo.com>
2023-06-01 16:26:36 +0300

    setup

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+// import "@fortawesome/fontawesome-svg-core/styles.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+reportWebVitals();
