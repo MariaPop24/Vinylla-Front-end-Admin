6feed1694114f90051235c7a65c000c9f341c2fb
Pop Maria <popmaria461@yahoo.com>
2023-06-05 12:52:28 +0300

    added view report modal

--- src/components/organisms/ViewReportModal/ViewReportModal.tsx
+++ src/components/organisms/ViewReportModal/ViewReportModal.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./ViewReportModal.scss";
 import Modal from "../Modal/Modal";
 import Button from "../../atoms/Button/Button";
+import axios from "axios";
+import { formatDateDiscount } from "../../../utils/formatDateDiscount";
 
 const ViewReportModal = ({
   setIsModalDisplayed,
@@ -12,6 +14,29 @@ const ViewReportModal = ({
   reportId: string;
   fetchReports: any;
 }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [report, setReport] = useState<any>();
+
+  const fetchReportData = async () => {
+    try {
+      setIsLoading(true);
+      const response = await axios.get(
+        `http://localhost:8000/api/reports/getReport/${reportId}`
+      );
+      const report = response.data.report;
+      console.log(report);
+      setReport(report);
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchReportData();
+  }, []);
+
   return (
     <Modal setIsModalDisplayed={setIsModalDisplayed}>
       <div className="add-discount--container">
@@ -25,6 +50,28 @@ const ViewReportModal = ({
             onClick={() => setIsModalDisplayed(false)}
           />
         </div>
+        {report && (
+          <div className="view-discount--info">
+            <div className="view-discount--item">
+              <p className="view-discount--label">reason</p>
+              <p className="view-discount--value">{report.reportReason}</p>
+            </div>
+            <div className="view-discount--item">
+              <p className="view-discount--label">reporter</p>
+              <p className="view-discount--value">{report.reporterAddress}</p>
+            </div>
+            <div className="view-discount--item">
+              <p className="view-discount--label">author</p>
+              <p className="view-discount--value">{report.authorAddress}</p>
+            </div>
+            <div className="view-discount--item">
+              <p className="view-discount--label">sent on</p>
+              <p className="view-discount--value">
+                {formatDateDiscount(report.createdAt)}
+              </p>
+            </div>
+          </div>
+        )}
       </div>
     </Modal>
   );
