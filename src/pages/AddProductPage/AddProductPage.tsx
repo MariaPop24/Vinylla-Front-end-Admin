f35ae0f6559afe286e6703af662db81632fd8576
Pop Maria <popmaria461@yahoo.com>
2023-09-12 00:02:16 +0300

    finished ui

--- src/pages/AddProductPage/AddProductPage.tsx
+++ src/pages/AddProductPage/AddProductPage.tsx
@@ -9,6 +9,9 @@ import { FormattedMessage } from "react-intl";
 
 const AddProductPage = () => {
   const [inputs, setInputs] = useState([{ value: "" }]);
+  const [details, setDetails] = useState([{ label: "", value: "" }]);
+  const [images, setImages] = useState([{ link: "" }]);
+  const [awards, setAwards] = useState([{ award: "" }]);
 
   const handleChange = (index: any, e: any) => {
     const newInputs = [...inputs];
@@ -22,10 +25,40 @@ const AddProductPage = () => {
     setInputs(newInputs);
   };
 
+  const handleAddDetailsInput = () => {
+    const newInputs = [...details];
+    newInputs.push({ label: "", value: "" });
+    setDetails(newInputs);
+  };
+  const handleAddImagesInput = () => {
+    const newInputs = [...images];
+    newInputs.push({ link: "" });
+    setImages(newInputs);
+  };
+  const handleAddAwardsInput = () => {
+    const newInputs = [...awards];
+    newInputs.push({ award: "" });
+    setAwards(newInputs);
+  };
   const handleRemoveInput = (index: any) => {
     const newInputs = [...inputs];
     newInputs.splice(index, 1);
     setInputs(newInputs);
+  };
+  const handleRemoveDetailsInput = (index: any) => {
+    const newInputs = [...details];
+    newInputs.splice(index, 1);
+    setDetails(newInputs);
+  };
+  const handleRemoveImagesInput = (index: any) => {
+    const newInputs = [...images];
+    newInputs.splice(index, 1);
+    setImages(newInputs);
+  };
+  const handleRemoveAwardsInput = (index: any) => {
+    const newInputs = [...awards];
+    newInputs.splice(index, 1);
+    setAwards(newInputs);
   };
 
   const handleSubmit = (e: any) => {
@@ -62,7 +95,7 @@ const AddProductPage = () => {
         <span className="add-product--label">description*: </span>
         <textarea className="add-product--textarea" maxLength={3000}></textarea>
       </div>
-      <div className="add-product--section-text">presentation images</div>
+      <div className="add-product--section-text">track list</div>
       <form onSubmit={handleSubmit}>
         {inputs.map((input: any, index: number) => (
           <div key={index}>
@@ -104,14 +137,136 @@ const AddProductPage = () => {
                 iconClassName="icon"
                 hasIconLeft={true}
                 iconLeft={require("../../assets/icons/PlusIcon.png")}
-                name={<FormattedMessage id="pages.allProducts.add-btn" />}
+                name="add song"
                 onClick={handleAddInput}
               />
             )}
           </div>
         ))}
-        <button type="submit">Submit</button>
       </form>
+      <div className="add-product--section-text">presentation images</div>
+      {images.map((input: any, index: number) => (
+        <div key={index}>
+          <div className="add-product--field-song clasa1">
+            <span className="add-product--label-song">link*: </span>
+            <TextInput
+              inputClassName="input-primary-style add-product--input"
+              type={InputType.TEXT}
+              value={input.value}
+              onChange={(e: any) => handleChange(index, e)}
+            />
+
+            {index !== images.length - 1 && (
+              <a
+                className="add-product--link"
+                onClick={() => handleRemoveImagesInput(index)}
+              >
+                remove
+              </a>
+            )}
+          </div>
+
+          {index === images.length - 1 && (
+            <Button
+              className="clasa1 btn-secondary-style "
+              iconClassName="icon"
+              hasIconLeft={true}
+              iconLeft={require("../../assets/icons/PlusIcon.png")}
+              name="add image"
+              onClick={handleAddImagesInput}
+            />
+          )}
+        </div>
+      ))}
+      <div className="add-product--section-text">
+        details (label, format, etc)
+      </div>
+      {details.map((input: any, index: number) => (
+        <div key={index}>
+          <div className="add-product--field-song clasa1">
+            <span className="add-product--label-song">label*: </span>
+            <TextInput
+              inputClassName="input-primary-style add-product--input"
+              type={InputType.TEXT}
+              value={input.value}
+              onChange={(e: any) => handleChange(index, e)}
+            />
+            <span className="add-product--label-song">value*: </span>
+            <TextInput
+              inputClassName="input-primary-style add-product--input"
+              type={InputType.TEXT}
+              value={input.value}
+              onChange={(e: any) => handleChange(index, e)}
+            />
+
+            {index !== details.length - 1 && (
+              <a
+                className="add-product--link"
+                onClick={() => handleRemoveDetailsInput(index)}
+              >
+                remove
+              </a>
+            )}
+          </div>
+
+          {index === details.length - 1 && (
+            <Button
+              className="clasa1 btn-secondary-style "
+              iconClassName="icon"
+              hasIconLeft={true}
+              iconLeft={require("../../assets/icons/PlusIcon.png")}
+              name="add item"
+              onClick={handleAddDetailsInput}
+            />
+          )}
+        </div>
+      ))}
+      <div className="add-product--section-text">awards</div>
+      {awards.map((input: any, index: number) => (
+        <div key={index}>
+          <div className="add-product--field-song clasa1">
+            <span className="add-product--label-song">award name: </span>
+            <TextInput
+              inputClassName="input-primary-style add-product--input"
+              type={InputType.TEXT}
+              value={input.value}
+              onChange={(e: any) => handleChange(index, e)}
+            />
+
+            {index !== awards.length - 1 && (
+              <a
+                className="add-product--link"
+                onClick={() => handleRemoveAwardsInput(index)}
+              >
+                remove
+              </a>
+            )}
+          </div>
+
+          {index === awards.length - 1 && (
+            <Button
+              className="clasa1 btn-secondary-style "
+              iconClassName="icon"
+              hasIconLeft={true}
+              iconLeft={require("../../assets/icons/PlusIcon.png")}
+              name="add award"
+              onClick={handleAddAwardsInput}
+            />
+          )}
+        </div>
+      ))}
+      <div className="add-product--buttons-container">
+        <Button
+          type={ButtonType.Button}
+          name="back"
+          className="btn-secondary-style"
+        />
+        <Button
+          type={ButtonType.Submit}
+          name="add product"
+          className="btn-primary-style clasa2"
+        />
+      </div>
     </div>
   );
 };
