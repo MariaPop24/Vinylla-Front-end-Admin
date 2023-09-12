c8b40782dc3d7371c4b17a2c45f2101e9436f9d8
Pop Maria <popmaria461@yahoo.com>
2023-09-12 08:33:52 +0300

    finished orders page

--- src/pages/OrdersPage/OrdersPage.tsx
+++ src/pages/OrdersPage/OrdersPage.tsx
@@ -1,8 +1,108 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./OrdersPage.scss";
+import TextBlock from "../../components/molecules/TextBlock/TextBlock";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { BeatLoader } from "react-spinners";
+import { FormattedMessage } from "react-intl";
+import Tooltip from "../../components/atoms/Tooltip/Tooltip";
+import Button from "../../components/atoms/Button/Button";
 
 const OrdersPage = () => {
-  return <div></div>;
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const [orders, setOrders] = useState([]);
+  const user = localStorage.getItem("usersData");
+
+  const fetchOrders = async () => {
+    try {
+      setIsLoading(true);
+      const response = await axios.get(
+        "http://localhost:8000/api/orders/getOrders"
+      );
+      const orders = response.data.orders;
+      setOrders(orders);
+      console.log(orders);
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+    }
+  };
+
+  function formatDateToDDMMYYYY(dateString: any) {
+    const date = new Date(dateString);
+
+    const day = String(date.getDate()).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0"); // Note: Month is zero-based
+    const year = date.getFullYear();
+
+    return `${day}-${month}-${year}`;
+  }
+
+  useEffect(() => {
+    fetchOrders();
+  }, []);
+
+  useEffect(() => {
+    if (user === null) {
+      navigate("/login");
+    }
+  }, [user, navigate]);
+  return (
+    <div className="all-products--container">
+      <TextBlock text="here are all the orders" />
+      <div className="all-product--albums">
+        {isLoading ? (
+          <div className="all-products--spinner">
+            <BeatLoader />
+          </div>
+        ) : (
+          <div className="all-products--list">
+            {orders && orders.length > 0 ? (
+              orders.map((item: any, index: number) => (
+                <div key={index}>
+                  <div key={index} className="order-card--container">
+                    <div className="order-card--info">
+                      <div className="order-card--number">
+                        tracking number: {item.trackingNumber}
+                      </div>
+                      <div className="order-card--details">
+                        <span className="order-card--status">
+                          {item.status}
+                        </span>{" "}
+                        | {formatDateToDDMMYYYY(item.deliveryDate)} | $
+                        {item.orderPrice}
+                      </div>
+                      <div className="order-card--details">
+                        {item.addressFirstName +
+                          " " +
+                          item.addressLastName +
+                          ", " +
+                          item.addressPhone}
+                      </div>
+                      <div className="order-card--details">
+                        {item.addressCountry +
+                          " " +
+                          item.addressRegion +
+                          ", " +
+                          item.address +
+                          ", " +
+                          item.addressZipCode}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))
+            ) : (
+              <div className="all-products--no-results">
+                <FormattedMessage id="pages.allProducts.no-results" />
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default OrdersPage;
